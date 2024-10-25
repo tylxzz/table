958499dd0c7daddf16f311ec43bcb3cd6435d145
Varga Mark Bence <varga.mark@BOLYAI.LOCAL>
2024-10-25 10:09:57 +0200

    refactor array

---
+++ functions.js
@@ -0,0 +1,76 @@
+/**
+ * 
+ * @param {'td' | 'th'} tagName 
+ * @param {string} innerHTML 
+ * @param {HTMLTableRowElement} parent 
+ * @returns {HTMLTableCellElement}
+ */
+function createTableCell(tagName, innerHTML, parent){
+    console.log(parent)
+    const cell = document.createElement(tagName);
+    cell.innerHTML = innerHTML;
+    parent.appendChild(cell);
+    return cell;
+}
+
+function createHTMLElement(tag, id, parent){
+    const elem = document.createElement(tag);
+    elem.id = id;
+    parent.appendChild(elem);
+}
+
+function createHTMLElementWithParentId(tag, id, parentid){
+    const parent = document.getElementById(parentid);
+    if (parent != undefined){
+        createHTMLElement(tag, id, parent);
+    }  
+}
+
+function renderTableHeader(){
+    const parent = document.getElementById('persontr');
+    createTableCell('th', 'Vezetéknév', parent)
+    const keresztnev = createTableCell('th', 'Keresztnév', parent);
+    keresztnev.colSpan;
+    createTableCell('th', 'Házas', parent);
+    createTableCell('th', 'Állat', parent);
+}
+
+function renderTable(personarray) {
+    const tbody = document.getElementById('persontbody')
+    for (const person of personarray) {
+        const row = document.createElement('tr');
+        tbody.appendChild(row);
+        const fn = document.createElement('td');
+        const fn1 = document.createElement('td');
+        fn.innerHTML = person.firstname1
+        fn1.innerHTML = person.firstname2
+        if (person.firstname2){
+            fn.innerHTML += " " + person.firstname2;
+        }
+        const ln = document.createElement('td');
+    
+        const marriedTableBodyrow = document.createElement('td');
+        const petTableBodyRow = document.createElement('td');
+    
+        ln.innerHTML = person.lastname;
+        marriedTableBodyrow.innerHTML = person.married? "Igen":"Nem";
+        petTableBodyRow.innerHTML = person.pet;
+    
+        row.appendChild(ln);
+        row.appendChild(fn);
+        row.appendChild(marriedTableBodyrow);
+        row.appendChild(petTableBodyRow);
+
+    
+        row.addEventListener('click', function(e) {
+            console.log("clicked" + lastname);
+            const selectedrow = tbody.querySelector('.selected');
+            if (selectedrow != undefined) {
+                selectedrow.classList.remove('selected')
+            }
+            e.currentTarget.classList.add('selected');
+        });
+    };
+}
+
+// Házi felkommentezés
--- main.js
+++ main.js
@@ -29,23 +29,16 @@ let array = [
     },
 ]
 
-const table = document.createElement('table');
-const tableheader = document.createElement('thead');
-table.appendChild(tableheader);
-const tableheaderRow = document.createElement('tr');
-tableheader.appendChild(tableheaderRow);
+createHTMLElement('table', 'persontable', document.body);
+createHTMLElementWithParentId('thead', 'personthead', 'persontable')
+createHTMLElementWithParentId('tr', 'persontr', 'personthead')
+renderTableHeader('persontr')
+createHTMLElementWithParentId('tbody', 'persontbody', 'persontable')
 
-createTableCell('th', 'Vezetéknév', tableheaderRow);
-createTableCell('th', 'Keresztnév', tableheaderRow);
-createTableCell('th', 'Házas', tableheaderRow);
-createTableCell('th', 'Állat', tableheaderRow);
-
-document.body.appendChild(table);
-const tbody = document.createElement('tbody');
-table.appendChild(tbody);
 
 const form = document.getElementById('form');
 form.addEventListener('submit', function(e){
+    const tbody = document.getElementById('persontbody')
     tbody.innerHTML = '';
     e.preventDefault();
     const lastname = document.getElementById('lastname');
@@ -74,64 +67,13 @@ form.addEventListener('submit', function(e){
         });
     }
 
-    renderTable();
+    renderTable(array);
     
     console.log(array);
     form.reset();
 });
 
-renderTable();
-
-function renderTable() {
-    for (const person of array) {
-        const row = document.createElement('tr');
-        tbody.appendChild(row);
-        const fn = document.createElement('td');
-        const fn1 = document.createElement('td');
-        fn.innerHTML = person.firstname1
-        fn1.innerHTML = person.firstname2
-        if (person.firstname2){
-            fn.innerHTML += " " + person.firstname2;
-        }
-        const ln = document.createElement('td');
-    
-        const marriedTableBodyrow = document.createElement('td');
-        const petTableBodyRow = document.createElement('td');
-    
-        ln.innerHTML = person.lastname;
-        marriedTableBodyrow.innerHTML = person.married? "Igen":"Nem";
-        petTableBodyRow.innerHTML = person.pet;
-    
-        row.appendChild(ln);
-        row.appendChild(fn);
-        row.appendChild(marriedTableBodyrow);
-        row.appendChild(petTableBodyRow);
-
-    
-        row.addEventListener('click', function(e) {
-            console.log("clicked" + lastname);
-            const selectedrow = tbody.querySelector('.selected');
-            if (selectedrow != undefined) {
-                selectedrow.classList.remove('selected')
-            }
-            e.currentTarget.classList.add('selected');
-        });
-    };
-}
-
-/**
- * 
- * @param {'td' | 'th'} tagName 
- * @param {string} innerHTML 
- * @param {HTMLTableRowElement} parent 
- * @returns {HTMLTableCellElement}
- */
-
-function createTableCell(tagName, innerHTML, parent){
-    const cell = document.createElement(tagName);
-    cell.innerHTML = innerHTML;
-    parent.appendChild(cell);
-}
+renderTable(array);
 
 function validateFields(lastname, firstname, pet){
     const errorMessages = form.querySelectorAll('.error')
@@ -160,3 +102,4 @@ function validateFields(lastname, firstname, pet){
 
     return result;
 }
+
